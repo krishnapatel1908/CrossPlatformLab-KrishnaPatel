03c7e06ee87f3e8316d2f1e425a7efd09d92b4c2
Krishna Patel <pate6047@sheridancollege.ca>
2026-02-18 14:19:53 -0500

    Add GitHub Actions workflows (dependent + multi-platform)

---
+++ src/crossplatformlab/FileManager.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crossplatformlab;
+
+public class FileManager {
+
+    public void printMessage() {
+        System.out.println("FileManager is running correctly!");
+    }
+
+    public static void main(String[] args) {
+        FileManager fm = new FileManager();
+        fm.printMessage();
+    }
+}
