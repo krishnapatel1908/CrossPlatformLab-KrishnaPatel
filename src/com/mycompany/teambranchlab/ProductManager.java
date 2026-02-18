d776d138e15a1ad22950d60ed45ea48ac84a1535
Krishna Patel <pate6047@sheridancollege.ca>
2026-02-18 14:41:13 -0500

    Add GitHub Actions workflows (dependent + multi-platform)

---
+++ src/com/mycompany/teambranchlab/ProductManager.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.teambranchlab;
+
+/**
+ *
+ * @author patel
+ */
+public class ProductManager {
+    public String getProductName() {
+        return "Sample Product, in starting code.";
+    }
+}
